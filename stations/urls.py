d3dcf6351639c0b728fa315c2c55af7bc00385be
Lauri Larjo <laurilarjo@gmail.com>
2010-11-28 00:37:11 +0200

    poistetaan pyc:it reposta. eclipsessa? projekti toimii nyt.

--- stations/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from api.models import Station
-
-urlpatterns = patterns('stations.views',
-    (r'^$', 'index'),
-    (r'^all/$', 'all'),
-    (r'^(?P<station_id>\d+)/$', 'details'),
-)
